0cb33d73cbfc301c68e1bbb8f1dc2f39de8ded08
MichaelWoldu <37816318+MichaelWoldu@users.noreply.github.com>
2021-06-23 15:11:57 +0100

    Update Db

--- code/mysql/db.php
+++ code/mysql/db.php
@@ -1,8 +1,15 @@
-<?php
-/* Database connection settings */
-$host = 'localhost';
-$user = 'dissertationLogin';
-$pass = '091164383707572306304pass';
-$db = 'gerthireddb';
-$mysqli = new mysqli($host,$user,$pass, $db) or die($mysqli->error);
+<?php
+
+/* Database connection settings */
+
+$host = 'localhost';
+
+$user = 'dissertationLogin';
+
+$pass = '';
+
+$db = 'gerthireddb';
+
+$mysqli = new mysqli($host,$user,$pass, $db) or die($mysqli->error);
+
 ?>
