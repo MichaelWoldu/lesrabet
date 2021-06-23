0f43b2564d64b8ca415dfd8bf43dbb956b6d639d
MichaelWoldu <37816318+MichaelWoldu@users.noreply.github.com>
2021-06-23 15:12:33 +0100

    Update MySQL

--- code/mysql/mysql.php
+++ code/mysql/mysql.php
@@ -1,15 +1,29 @@
-<?php
-$servername= "localhost";
-$username = "root";
-$password = "0911206639";
-
-// create connection 
-
-$conn = mysqli_connect($servername, $username, $password);
-
-// Check Connection 
-if (!$conn) {
-	die("Connection failed: " . mysqli_connect_error());
-}
-echo "Connection Succesful";
+<?php
+
+$servername= "localhost";
+
+$username = "root";
+
+$password = "";
+
+
+
+// create connection 
+
+
+
+$conn = mysqli_connect($servername, $username, $password);
+
+
+
+// Check Connection 
+
+if (!$conn) {
+
+	die("Connection failed: " . mysqli_connect_error());
+
+}
+
+echo "Connection Succesful";
+
 ?>
